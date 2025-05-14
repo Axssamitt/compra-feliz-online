f758fb98f21bd659df509687d83de8346cacf66e
Axssamitt <jsrmturk@gmail.com>
2025-05-13 21:13:00 -0300

    Update CreateAdminUser.tsx

--- src/pages/CreateAdminUser.tsx
+++ src/pages/CreateAdminUser.tsx
@@ -10,8 +10,8 @@ import { toast } from "sonner";
 
 const CreateAdminUser: React.FC = () => {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('errejota.ecomerce@gmail.com');
-  const [password, setPassword] = useState('@Ro17524');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
 
   const handleCreateUser = async (e: React.FormEvent) => {
